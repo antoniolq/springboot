2a5408e2c4f24e43c58641408054bafb3bf642c1
Administrator <admin@example.com>
2022-09-05 20:34:56 +0800

    init

---
+++ src/main/java/org/example/entity/Student.java
@@ -0,0 +1,12 @@
+package org.example.entity;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+}
---
+++ src/main/java/org/example/test/Test.java
@@ -0,0 +1,19 @@
+package org.example.test;
+
+import org.example.entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        //传统方式
+        Student student = new Student();
+        System.out.println(student);
+
+        //IOC方式
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Student student1 = (Student) applicationContext.getBean("student");
+        System.out.println(student);
+    }
+}
