1a87acceb22f14de2a164714376111107fb7d181
Administrator <admin@example.com>
2022-09-05 21:55:02 +0800

    dev01

--- src/main/java/org/example/entity/Student.java
+++ src/main/java/org/example/entity/Student.java
@@ -1,9 +1,13 @@
 package org.example.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private Integer id;
     private String name;
--- src/main/java/org/example/test/Test.java
+++ src/main/java/org/example/test/Test.java
@@ -8,12 +8,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         //传统方式
-        Student student = new Student();
-        System.out.println(student);
+//        Student student = new Student();
+//        System.out.println(student);
 
         //IOC方式
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        Student student1 = (Student) applicationContext.getBean("student");
-        System.out.println(student);
+        Student student1 = (Student) applicationContext.getBean("student2");
+        System.out.println(student1);
     }
 }
